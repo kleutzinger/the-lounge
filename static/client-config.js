f79454a6f96afb0f3ea28df1e26c85ce452d55f6
kleutzinger <thedookmaster@gmail.com>
2020-04-06 12:30:36 -0700

    moved js to static folder

---
+++ static/client-config.js
@@ -0,0 +1,10 @@
+/** CONFIG **/
+var SIGNALING_SERVER = 'http://localhost';
+var USE_AUDIO = true;
+var USE_VIDEO = true;
+var DEFAULT_CHANNEL = 'some-global-channel-name';
+var MUTE_AUDIO_BY_DEFAULT = false;
+
+/** You should probably use a different stun server doing commercial stuff **/
+/** Also see: https://gist.github.com/zziuni/3741933 **/
+var ICE_SERVERS = [ { url: 'stun:stun.l.google.com:19302' } ];
