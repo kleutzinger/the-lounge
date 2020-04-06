d0e85612c9e136faa1b44003013be4ded42260ac
kleutzinger <thedookmaster@gmail.com>
2020-04-06 15:30:52 -0700

    yay

--- push-to-vps.sh
+++ push-to-vps.sh
@@ -1,5 +1,5 @@
 rsync -v -ah --delete \
-    --cvs-exclude --exclude-from="$(git -C ./ ls-files \
+    --cvs-exclude --exclude="node_modules" --exclude-from="$(git -C ./ ls-files \
         --exclude-standard -oi --directory >.git/ignores.tmp && \
         echo .git/ignores.tmp)" \
     ./ root@kevbot.xyz:/root/sites/the-lounge/ 
