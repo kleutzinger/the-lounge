074cc2d4b7ea7e60af451bae13b696eb2ebd4a71
kleutzinger <thedookmaster@gmail.com>
2020-04-06 12:21:34 -0700

    added static file serving

--- signaling-server.js
+++ signaling-server.js
@@ -3,41 +3,39 @@
 /**************/
 var PORT = 8080;
 
-
 /*************/
 /*** SETUP ***/
 /*************/
 
-const https = require("https"),
-fs = require("fs");
+const https = require('https'),
+  fs = require('fs');
 
 //const options = {
 //   key: fs.readFileSync("/etc/letsencrypt/live/jabdownsmash.com/privkey.pem"),
 //   cert: fs.readFileSync("/etc/letsencrypt/live/jabdownsmash.com/cert.pem")
 //};
 
-
-
 var express = require('express');
 var http = require('http');
-var bodyParser = require('body-parser')
-var main = express()
-var server = http.createServer(main)
+var bodyParser = require('body-parser');
+var app = express();
+var server = http.createServer(app);
 
 //var server = https.createServer(options, main)
-var io  = require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
 //io.set('log level', 2);
 
 server.listen(PORT, null, function() {
-    console.log("Listening on port " + PORT);
+  console.log('Listening on port ' + PORT);
 });
 //main.use(express.bodyParser());
 
-main.get('/', function(req, res){ res.sendFile(__dirname + '/client.html'); });
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/client.html');
+});
 // main.get('/index.html', function(req, res){ res.sendfile('newclient.html'); });
 // main.get('/client.html', function(req, res){ res.sendfile('newclient.html'); });
-
-
+app.use('/static', express.static('static'));
 
 /*************************/
 /*** INTERESTING STUFF ***/
@@ -56,100 +54,122 @@ var avatars = {};
  * information. After all of that happens, they'll finally be able to complete
  * the peer connection and will be streaming audio/video between eachother.
  */
-io.sockets.on('connection', function (socket) {
-    socket.channels = {};
-    sockets[socket.id] = socket;
+io.sockets.on('connection', function(socket) {
+  socket.channels = {};
+  sockets[socket.id] = socket;
 
-    console.log("["+ socket.id + "] connection accepted");
-    socket.on('disconnect', function () {
-        for (var channel in socket.channels) {
-            part(channel);
-        }
-        console.log("["+ socket.id + "] disconnected");
-        delete sockets[socket.id];
-        delete avatars[socket.id];
-    });
-
-
-    socket.on('join', function (config) {
-        console.log("["+ socket.id + "] join ", config);
-        var channel = config.channel;
-        var userdata = config.userdata;
-
-        if (channel in socket.channels) {
-            console.log("["+ socket.id + "] ERROR: already joined ", channel);
-            return;
-        }
-
-        if (!(channel in channels)) {
-            channels[channel] = {};
-        }
-
-        avatars[socket.id] = {"x": 0, "y": 0};
-
-        for (id in channels[channel]) {
-            channels[channel][id].emit('addPeer', {'peer_id': socket.id, 'should_create_offer': false, "position": avatars[socket.id]});
-            socket.emit('addPeer', {'peer_id': id, 'should_create_offer': true, "position": avatars[socket.id]});
-        }
-
-        channels[channel][socket.id] = socket;
-        socket.channels[channel] = channel;
-    });
-
-    function part(channel) {
-        console.log("["+ socket.id + "] part ");
-
-        if (!(channel in socket.channels)) {
-            console.log("["+ socket.id + "] ERROR: not in ", channel);
-            return;
-        }
-
-        delete socket.channels[channel];
-        delete channels[channel][socket.id];
-
-        for (id in channels[channel]) {
-            channels[channel][id].emit('removePeer', {'peer_id': socket.id});
-            socket.emit('removePeer', {'peer_id': id});
-        }
+  console.log('[' + socket.id + '] connection accepted');
+  socket.on('disconnect', function() {
+    for (var channel in socket.channels) {
+      part(channel);
     }
-    socket.on('part', part);
+    console.log('[' + socket.id + '] disconnected');
+    delete sockets[socket.id];
+    delete avatars[socket.id];
+  });
 
-    socket.on('relayICECandidate', function(config) {
-        var peer_id = config.peer_id;
-        var ice_candidate = config.ice_candidate;
-        console.log("["+ socket.id + "] relaying ICE candidate to [" + peer_id + "] ", ice_candidate);
+  socket.on('join', function(config) {
+    console.log('[' + socket.id + '] join ', config);
+    var channel = config.channel;
+    var userdata = config.userdata;
 
-        if (peer_id in sockets) {
-            sockets[peer_id].emit('iceCandidate', {'peer_id': socket.id, 'ice_candidate': ice_candidate});
-        }
-    });
-
-    socket.on('relaySessionDescription', function(config) {
-        var peer_id = config.peer_id;
-        var session_description = config.session_description;
-        console.log("["+ socket.id + "] relaying session description to [" + peer_id + "] ", session_description);
-
-        if (peer_id in sockets) {
-            sockets[peer_id].emit('sessionDescription', {'peer_id': socket.id, 'session_description': session_description});
-        }
-    });
-
-    socket.on('updatePosition', function(config) {
-        if (!(socket.id in avatars)){
-            console.log("socket id not found in avatars " + socket.id);
-            return;
-        }
-        avatars[socket.id]["x"] = config["x"];
-        avatars[socket.id]["y"] = config["y"];
-    });
-
-    function updatePeerAvatars() {
-        for (var peer_id in avatars) {
-            // if (peer_id == socket.id) continue;
-
-            socket.emit("updatePeerAvatars", {'peer_id':peer_id, "avatar":avatars[peer_id]});
-        }
-        setTimeout(updatePeerAvatars,200);
+    if (channel in socket.channels) {
+      console.log('[' + socket.id + '] ERROR: already joined ', channel);
+      return;
     }
-    updatePeerAvatars();
+
+    if (!(channel in channels)) {
+      channels[channel] = {};
+    }
+
+    avatars[socket.id] = { x: 0, y: 0 };
+
+    for (id in channels[channel]) {
+      channels[channel][id].emit('addPeer', {
+        peer_id: socket.id,
+        should_create_offer: false,
+        position: avatars[socket.id]
+      });
+      socket.emit('addPeer', {
+        peer_id: id,
+        should_create_offer: true,
+        position: avatars[socket.id]
+      });
+    }
+
+    channels[channel][socket.id] = socket;
+    socket.channels[channel] = channel;
+  });
+
+  function part(channel) {
+    console.log('[' + socket.id + '] part ');
+
+    if (!(channel in socket.channels)) {
+      console.log('[' + socket.id + '] ERROR: not in ', channel);
+      return;
+    }
+
+    delete socket.channels[channel];
+    delete channels[channel][socket.id];
+
+    for (id in channels[channel]) {
+      channels[channel][id].emit('removePeer', { peer_id: socket.id });
+      socket.emit('removePeer', { peer_id: id });
+    }
+  }
+  socket.on('part', part);
+
+  socket.on('relayICECandidate', function(config) {
+    var peer_id = config.peer_id;
+    var ice_candidate = config.ice_candidate;
+    console.log(
+      '[' + socket.id + '] relaying ICE candidate to [' + peer_id + '] ',
+      ice_candidate
+    );
+
+    if (peer_id in sockets) {
+      sockets[peer_id].emit('iceCandidate', {
+        peer_id: socket.id,
+        ice_candidate: ice_candidate
+      });
+    }
+  });
+
+  socket.on('relaySessionDescription', function(config) {
+    var peer_id = config.peer_id;
+    var session_description = config.session_description;
+    console.log(
+      '[' + socket.id + '] relaying session description to [' + peer_id + '] ',
+      session_description
+    );
+
+    if (peer_id in sockets) {
+      sockets[peer_id].emit('sessionDescription', {
+        peer_id: socket.id,
+        session_description: session_description
+      });
+    }
+  });
+
+  socket.on('updatePosition', function(config) {
+    if (!(socket.id in avatars)) {
+      console.log('socket id not found in avatars ' + socket.id);
+      return;
+    }
+    avatars[socket.id]['x'] = config['x'];
+    avatars[socket.id]['y'] = config['y'];
+  });
+
+  function updatePeerAvatars() {
+    for (var peer_id in avatars) {
+      // if (peer_id == socket.id) continue;
+
+      socket.emit('updatePeerAvatars', {
+        peer_id: peer_id,
+        avatar: avatars[peer_id]
+      });
+    }
+    setTimeout(updatePeerAvatars, 200);
+  }
+  updatePeerAvatars();
 });
